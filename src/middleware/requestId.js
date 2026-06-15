08af6ac3d3655e45ab382594c833b88bd73f96f0
P-r-e-m-i-u-m <amanbaba9404522@gmail.com>
2026-06-15 16:17:32 +0000

    feat: add X-Request-ID tracing middleware for distributed logging (closes #192)

--- src/middleware/requestId.js
+++ src/middleware/requestId.js
@@ -9,5 +9,5 @@ const requestId = (req, res, next) => {
   next();
 };
 
-module.exports = { requestId };  // Updated: 2026-06-05
-// build: 1780667517
+module.exports = { requestId };  // Updated: 2026-06-15
+// build: 1781540249
